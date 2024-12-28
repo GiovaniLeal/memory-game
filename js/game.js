d28eb4cfbc4698b5693c3c0e63c922ffd51a21e1
Giovani Leal <108373745+GiovaniLeal@users.noreply.github.com>
2024-12-28 14:50:53 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -173,7 +173,7 @@ const createCard = (character) => {
     const front = createElement ('div', 'face front');
     const back = createElement ('div', 'face back');
 
-    front.style.backgroundImage = `url('/images/${character}.PNG')`;
+    front.style.backgroundImage = `url('./images/${character}.PNG')`;
 
     card.appendChild(front);
     card.appendChild(back);
